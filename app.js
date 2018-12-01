5ead36805e20b7fd563483c15db9b7a2c929c92e
Abdul Elah Rizan <abdulelah.rizan@gmail.com>
2018-12-01 13:50:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app);
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(__dirname + '/site'));
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/index.html')
+});
+server.listen(3000);
+console.log("Server started.");
